60bb8b6ba846d71d6e6dba720ed758774b0ff94f
Jonathan Walker <jwsmasher@gmail.com>
2021-05-13 14:40:17 -0500

    Enemies can now dodge

--- Assets/Scripts/Ai/Steering.cs
+++ Assets/Scripts/Ai/Steering.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class Steering : MonoBehaviour
 {
+    public float avoidWeight = 1;
+    public float pathWeight = 1;
+    public float updateInterval = 1;
     NavMeshPath path;
     int index;
     CharacterMotor motor;
@@ -14,7 +17,7 @@ public class Steering : MonoBehaviour
         //behaviors = GetComponentsInChildren<SteeringBehavior>();
         motor = GetComponent<CharacterMotor>();
         path = new NavMeshPath();
-        yield return new WaitForSeconds(Random.Range(0,3));
+        yield return new WaitForSeconds(Random.Range(0,updateInterval));
         while (enabled)
         {
             Vector3 p = PlayerController.player.transform.position;
@@ -26,16 +29,8 @@ public class Steering : MonoBehaviour
 
     void Update()
     {
-        if (path.status != NavMeshPathStatus.PathComplete || index >= path.corners.Length)
-        {
-            motor.moveDir = Vector3.zero;
-            return;
-        }
-        Vector3 diff = path.corners[index] - transform.position;
-        diff.y = 0;
-        motor.moveDir = diff.normalized;
 
-        if (diff.sqrMagnitude < 0.1f) index ++;
+        motor.moveDir = (PathSteer() + AvoidSteer()).normalized;
     }
 
     Vector3 PathSteer()
@@ -46,13 +41,28 @@ public class Steering : MonoBehaviour
         diff.y = 0;
         if (diff.sqrMagnitude < 0.1f) index ++;
 
-        return diff.normalized;
+        return diff.normalized * pathWeight;
     }
 
     Vector3 AvoidSteer()
     {
         Vector3 avoidSteer = Vector3.zero;
-        return avoidSteer;
+        foreach (BulletController bullet in BulletController.bullets)
+        {
+            Vector3 diff = transform.position - bullet.transform.position;
+            float dist = diff.magnitude;
+            Vector3 dir = diff/dist;
+            Vector3 velo = bullet.GetComponent<Rigidbody>().velocity;
+            float speed = velo.magnitude;
+            float dot = Vector3.Dot(dir,velo/speed);
+            if (dot <= 0) continue;
+
+            float t = dist/speed;
+            Vector3 dest = bullet.transform.position + velo * t;
+            dest.y = 0;
+            avoidSteer += transform.position - dest;
+        }
+        return avoidSteer.normalized * avoidWeight;
         
     }
 }
